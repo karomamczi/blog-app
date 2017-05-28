fabd38056a0005b8f3d9fd14ce3712167e31520c
Karolina Mamczarz <karolina.mamczarz@op.pl>
2017-05-29 00:05:40 +0200

    Respect .gitignore

--- myenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/darthvader/Desktop/projects_test/djangogirls/myenv/bin/python3.5
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
